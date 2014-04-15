cf081d07da71f424d811c5a9ddb2364651bf9b81
vinchi9 <micek.luki@seznam.cz>
2014-04-15 23:08:23 +0200

    Added hourly logging

--- meteoLib.py
+++ meteoLib.py
@@ -1,18 +1,5 @@
 from pymlab import config
 import time, json, requests
-from requests.auth import HTTPBasicAuth
-
-#### Sensor Configuration [START]#########################################
-#barometer = {"name": "barometer", "type": "altimet01" , "channel": 0, }
-#hum_temp = {"name": "hum_temp", "type": "sht25" , "channel": 1, }
-#wind_direction = {"name": "wind_direction", "type": "mag01" , "channel": 1, }
-#thermometer = {"name": "lts", "type": "lts01" , "channel": 2, }
-
-#barometer = cfg.get_device("barometer")
-#hum_temp = cfg.get_device("hum_temp")
-#wind_direction = cfg.get_device("wind_direction")
-#thermometer = cfg.get_device("thermometer")
-#### Sensor Configuration [END]###########################################
 
 
 class Meteostation:
@@ -24,9 +11,9 @@ class Meteostation:
         self.configBus = JSONconfig["configBus"]
         self.translationConfig = JSONconfig["translationConfig"]
         self.logFileName = JSONconfig["logFileName"]
-	self.stationName = JSONconfig["stationName"]
-	self.latitude = JSONconfig["latitude"]
-	self.longitude = JSONconfig["longitude"]
+        self.stationName = JSONconfig["stationName"]
+        self.latitude = JSONconfig["latitude"]
+        self.longitude = JSONconfig["longitude"]
 
         cfg = config.Config(i2c={"port":1}, bus=self.configBus)
         cfg.initialize()
@@ -37,7 +24,7 @@ class Meteostation:
 
         time.sleep(0.5)
 
-    def getData(self,requestList="all"):
+    def getData(self,requestList="all"):        # returns requested sensor data
         outputList = {}
         outputList["time"] = int(time.time())
         if requestList == "all":
@@ -57,18 +44,18 @@ class Meteostation:
             self.Devices[sensorName].route()
             data = self.Devices[sensorName].get_tp()
             return {"0":data[0],"1":data[1]/100}
-        elif sensorName == "lts":
-            self.Devices[sensorName].route()
-            return list(self.Devices[sensorName].get_temp())
 
-    def log(self,dataDict,logFileName=""):
+    def log(self,dataDict,logFileName=""):      # logging function
         if logFileName == "":
             logFileName = self.logFileName
+            
+        logFileName = time.strftime("%d-%m-%Y:%H-", time.localtime()) + logFileName
+
         try:
             with open(logFileName,"r") as f:
                 pass
         except:
-            self.generateLogFile(logFileName)
+            self.__generateLogFile(logFileName)
 
         try:
             with open(logFileName,"r") as f:
@@ -80,21 +67,24 @@ class Meteostation:
         except:
             print "Logging failed"
             
-    def generateLogFile(self,logFileName):
+    def __generateLogFile(self,logFileName):      # generator of a log file
         defaultLog = []
-        with open(logFileName,"w") as f:
-            f.write(json.dumps(defaultLog))
+        try:
+            with open(logFileName,"w") as f:
+                f.write(json.dumps(defaultLog))
+        except:
+            print "Cannot generate log file"
 
-    def sendData(self,username,password,sendDict):
-	sendData = self.translateToPOST(sendDict)
-        result = requests.post("http://openweathermap.org/data/post",auth=HTTPBasicAuth(username,password),data=sendData)
+    def sendData(self,username,password,sendDict):      # sends data to openweathermap.com
+        sendData = self.translateToPOST(sendDict)
+        result = requests.post("http://openweathermap.org/data/post",auth=(username,password),data=sendData)
         JSONresult = result.json()
         if JSONresult["cod"] == "200":
             return (True,JSONresult)
         else:
             return  (False,JSONresult)
 
-    def translateToPOST(self,sendDict):    # [ OWM.com name, sendDict name, sendDict name 2 ]
+    def translateToPOST(self,sendDict):    # translates sensor values to POST request format
         payload = {}
         for itemKey in sendDict.keys():
             for transList in self.translationConfig:
@@ -102,9 +92,9 @@ class Meteostation:
                     payload[transList[0]] = str(round(sendDict[itemKey],2))
                 elif not transList[2] == "" and itemKey == transList[1]:
                     payload[transList[0]] = str(round(sendDict[itemKey][transList[2]],2))
-	if self.stationName:
-	    payload["name"] = str(self.stationName)
-	if self.latitude and self.longitude:
-	    payload["lat"] = str(self.latitude)
-	    payload["long"] = str(self.longitude)
+        if self.stationName:
+            payload["name"] = str(self.stationName)
+        if self.latitude and self.longitude:
+            payload["lat"] = str(self.latitude)
+            payload["long"] = str(self.longitude)
         return payload
